11bbf5fcb3e9a84e0104b7a2985cf9b705c0ec61
曲木车格 <qumuchege@qumuchegedeMacBook-Pro.local>
2023-05-02 15:36:21 +0800

    init

---
+++ apps/www/src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Button } from "components";
+import "./index.scss";
+
+const t = "0";
+
+const y = 0
+
+function App(){
+	return <div>
+		<h1 className='title'>www</h1>
+		<Button/>
+	</div>;
+}
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />);
